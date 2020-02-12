7677ce79f74cadf765180a98fde23d96708b72eb
laurentiu-meghesan <laurentiu.meghesan@gmail.com>
2020-02-12 19:29:33 +0200

    displaying tasks from api

--- js/tasks.js
+++ js/tasks.js
@@ -8,18 +8,33 @@ window.ToDoList = {
             method: "GET"
         }).done(function (response) {
             console.log(response);
+            ToDoList.displayTasks(JSON.parse(response));
         })
     },
 
     getTaskRow: function (task) {
+        //spread operator (...)
+        let formattedDeadline = new Date(...task.deadline).toLocaleDateString("ro");
+
+        let checkedAttribute = task.done ? " checked": "";
+
         return `<tr>
             <td>${task.description}</td>
-            <td>${task.deadline}</td>
-            <td><input type="checkbox" data-id=${task.id} class="mark-done"/></td>
+            <td>${formattedDeadline}</td>
+            <td><input type="checkbox" data-id=${task.id} class="mark-done" ${checkedAttribute}/></td>
             <td><a href="#" data-id=${task.id} class="delete-task">
                 <i class="fas fa-trash-alt"></i>
             </a></td>
         </tr>`
+    },
+
+    displayTasks: function (tasks) {
+        // weak-types (javascript) vs strong-typed (java)
+        var tableBody = '';
+
+        tasks.forEach(task => tableBody += ToDoList.getTaskRow(task))
+
+        $("#tasks-table tbody").html(tableBody);
     }
 
 };
